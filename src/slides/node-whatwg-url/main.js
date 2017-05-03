1149243808f07cb6febc8dbd0a3d6da3e9f76735
Yuta Hiroto <git@about-hiroppy.com>
2017-05-03 20:49:27 +0900

    feat(ndoe-whatwg-url): add social buttons

--- src/slides/node-whatwg-url/main.js
+++ src/slides/node-whatwg-url/main.js
@@ -6,6 +6,7 @@ import insertSlide from '../../lib/insert-slide';
 import AppContainer from '../../lib/AppContainer';
 import fetchSlides, { fetchCommon } from '../../lib/fetch-slides';
 import setupBespoke from '../../lib/setup-bespoke';
+import addSocialButtons from '../../lib/add-social-buttons';
 import '../../lib/setup-hljs';
 import './style.css';
 import '../../common/style/common.css';
@@ -16,6 +17,7 @@ const profile = fetchCommon()['self-introduction.md'];
 
 const App = () => {
   const slides = insertSlide(slidesInfo.slides, profile, 1);
+  slides[0] = addSocialButtons(slides[0]);
 
   return (
     <AppContainer slides={slides} />
