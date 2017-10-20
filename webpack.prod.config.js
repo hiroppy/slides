18f09f726d54de57716d2ef8ba0321a861bbd418
Yuta Hiroto <git@about-hiroppy.com>
2017-10-21 08:26:26 +0900

    chore(babel): modify browsers version and make use of babel-minify

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const webpack       = require('webpack');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 const OfflinePlugin = require('offline-plugin');
 
 module.exports = {
   plugins: [
     new OfflinePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false
-    }),
+    new MinifyPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin()
   ]
