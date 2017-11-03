be0fb3dd106ae6c6e8300ee118a9f93deaea68db
Yuta Hiroto <git@about-hiroppy.com>
2017-11-03 13:20:38 +0900

    feat(presenter): create host page

--- src/lib/ContentView/Host.js
+++ src/lib/ContentView/Host.js
@@ -3,8 +3,27 @@
  */
 
 import React from 'react';
+import '../../common/style/host.css';
 
 class Host extends React.Component {
+  constructor(props) {
+    super();
+
+    this.slides = props.slides; // TODO: separate from current reference
+
+    const lastPage = { // TODO: display text
+      meta: {
+        note      : '',
+        className : '',
+        background: 'default'
+      },
+      content: '<h2>finish</h2>'
+    };
+
+    this.slides.shift(); // delete the first page
+    this.slides = [...this.slides, lastPage];
+  }
+
   componentWillReceiveProps(p) {
 
     // to load when bespoke loading is completed
@@ -22,20 +41,34 @@ class Host extends React.Component {
 
   render() {
     return (
-      <article>
-        {
-          this.props.slides.map((slide, i) => {
-            return (
-              <section
-                key={i /* fix */}
-                className={slide.meta.className}
-                data-bespoke-backdrop={slide.meta.background}
-                dangerouslySetInnerHTML={{ __html: slide.context }}
-              />
-            );
-          })
-        }
-      </article>
+      <div className="host-container">
+        <div className="host-note">
+          {
+            window.slide && window.slide.bespoke.slide() - 1 >= 0 ? (
+              <pre>
+                {
+                  this.slides[window.slide.bespoke.slide() - 1].meta.note
+                }
+              </pre>
+            ) : null
+          }
+        </div>
+        <div className="host-info">
+          <h2>next</h2>
+          <article className="host-next-slide">
+            {
+              this.slides.map((slide, i) => (
+                <section
+                  key={i /* fix */}
+                  className={slide.meta.className}
+                  data-bespoke-backdrop={slide.meta.background}
+                  dangerouslySetInnerHTML={{ __html: slide.context }}
+                />
+              ))
+            }
+          </article>
+        </div>
+      </div>
     );
   }
 }
