69e26ef9f85cfa325d14412634a14606c88e40a7
Yuta Hiroto <git@about-hiroppy.com>
2018-05-02 00:32:26 +0900

    refactor(how-to-manage-the-document-of-Node.js): make use of fusuma

---
+++ slides/how-to-manage-the-document-of-Node.js/.fusumarc.js
@@ -0,0 +1,15 @@
+const base = require("../../.fusumarc");
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    description: "Explain how Node.js's document is managed",
+    thumbnail: "https://avatars3.githubusercontent.com/u/9950313?s=200&v=4"
+  },
+  slide: {
+    theme: "js"
+  },
+  extends: {
+    css: "style.css"
+  }
+};
