e25bc8f15b4b4e6cfde26f456b805706ad795e65
Yuta Hiroto <git@about-hiroppy.com>
2018-05-02 19:17:27 +0900

    refactor(node-whatwg-url): make use of fusuma

---
+++ slides/node-whatwg-url/.fusumarc.js
@@ -0,0 +1,15 @@
+const base = require("../../.fusumarc");
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    description: "whatwg-url in Node",
+    thumbnail: "https://avatars3.githubusercontent.com/u/9950313?s=200&v=4"
+  },
+  slide: {
+    theme: "js"
+  },
+  extends: {
+    css: "style.css"
+  }
+};
