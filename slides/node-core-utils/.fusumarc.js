0d5e88a919acf0e0f67425688a762efdcf666960
Yuta Hiroto <git@about-hiroppy.com>
2018-05-02 19:12:45 +0900

    refactor(node-core-utils): make use of fusuma

---
+++ slides/node-core-utils/.fusumarc.js
@@ -0,0 +1,15 @@
+const base = require("../../.fusumarc");
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    description: "Introduce node-core-utils.",
+    thumbnail: "https://avatars3.githubusercontent.com/u/9950313?s=200&v=4"
+  },
+  slide: {
+    theme: "js"
+  },
+  extends: {
+    css: "style.css"
+  }
+};
