3e704765006a17fc2b814046bdf15dae5c07d1d4
Yuta Hiroto <git@about-hiroppy.com>
2018-05-02 18:30:12 +0900

    refactor(nicohaco): make use of fusuma

---
+++ slides/nicohaco/.fusumarc.js
@@ -0,0 +1,15 @@
+const base = require("../../.fusumarc");
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    description: "NicoHacoというNicoBoxのPC版アプリを作った",
+    thumbnail: ""
+  },
+  slide: {
+    theme: "js"
+  },
+  extends: {
+    css: "style.css"
+  }
+};
