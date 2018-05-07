ff347f484d672c66d2e8d0103c8735852107633d
Yuta Hiroto <git@about-hiroppy.com>
2018-05-07 09:24:45 +0900

    feat(performance-timing-api-with-node.js): make use of fusuma

---
+++ slides/performance-timing-api-with-node.js/.fusumarc.js
@@ -0,0 +1,15 @@
+const base = require("../../.fusumarc");
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    description: "introduce performance timing api of Node.js",
+    thumbnail: "https://avatars3.githubusercontent.com/u/9950313?s=200&v=4"
+  },
+  slide: {
+    theme: "js"
+  },
+  extends: {
+    css: "style.css"
+  }
+};
