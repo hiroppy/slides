8f853caafb78b9ad904405af73b13f30bcf1b753
Yuta Hiroto <git@about-hiroppy.com>
2018-05-07 09:24:45 +0900

    feat(performance-timing-api-with-node.js): make use of fusuma

--- .fusumarc.js
+++ .fusumarc.js
@@ -2,6 +2,7 @@ module.exports = {
   meta: {
     url: "https://slide.hiroppy.me",
     author: "Yuta Hiroto",
-    siteName: "hiroppy's slides"
+    siteName: "hiroppy's slides",
+    repositoryUrl: "https://github.com/hiroppy/slides"
   }
 };
