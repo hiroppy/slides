faba0a5fea2731c2cf623db4c49816d5a6dccf78
Yuta Hiroto <git@about-hiroppy.com>
2018-05-13 12:35:42 +0900

    feat(command): add init command

---
+++ template/.fusumarc.js
@@ -0,0 +1,15 @@
+const base = require("../../.fusumarc");
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    description: "",
+    thumbnail: ""
+  },
+  slide: {
+    theme: "js"
+  },
+  extends: {
+    css: "style.css"
+  }
+};
