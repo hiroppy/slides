0bfc403f83a114090528f4a755421e85bdc954ce
Yuta Hiroto <git@about-hiroppy.com>
2018-07-14 12:58:31 +0900

    feat(future-of-node): add slides

---
+++ slides/future-of-node/.fusumarc.js
@@ -0,0 +1,16 @@
+const base = require('../../.fusumarc');
+
+module.exports = {
+  meta: {
+    ...base.meta,
+    name: 'Future of Node.js',
+    description: 'about feature of Node.js',
+    thumbnail: 'https://avatars3.githubusercontent.com/u/9950313?s=200&v=4'
+  },
+  slide: {
+    theme: 'nebula'
+  },
+  extends: {
+    css: 'style.css'
+  }
+};
---
+++ slides/future-of-node/slides/02-content.md
@@ -0,0 +1,5 @@
+<!-- contents -->
+
+## Content
+
+<br />
