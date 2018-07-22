7aa38bc976cb48e2f16ceff45b16ce7801dd6f71
Yuta Hiroto <git@about-hiroppy.com>
2018-07-23 08:32:27 +0900

    feat: delete url from .fusuma.js

--- .fusumarc.js
+++ .fusumarc.js
@@ -1,12 +1,8 @@
 module.exports = {
   meta: {
-    url: 'https://slide.hiroppy.me',
     author: 'Yuta Hiroto',
-    siteName: 'hiroppy\'s slides',
+    siteName: "hiroppy's slides",
     repositoryUrl: 'https://github.com/hiroppy/slides',
-    sns: [
-      'twitter',
-      'hatena'
-    ]
+    sns: ['twitter', 'hatena']
   }
 };
