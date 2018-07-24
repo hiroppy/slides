05c757d0140817bd95a4515675cd871c32eff081
Yuta Hiroto <git@about-hiroppy.com>
2018-07-24 23:43:58 +0900

    refactor(fusuma): delete targetBlank and use fusuma's targetBlank

--- slides/future-of-node/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', (e) => {
-  if (e.target.tagName === 'A') {
-    if (e.target.host === location.host || e.target.href === '') return;
-
-    e.preventDefault();
-    window.open(e.target.href, '_blank');
-  }
-});
