c2b86e0f74ee901bffc39019bef31261ee130f15
Yuta Hiroto <git@about-hiroppy.com>
2018-09-08 23:21:41 +0900

    feat(common): update self-introduction slide

---
+++ common/main.js
@@ -0,0 +1,5 @@
+// add font-awesome
+
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/css/brands.min.css';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
